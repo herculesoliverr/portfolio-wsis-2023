5cbe2736f80d9da23ae39d9dccd02206cee5b2f7
Hercules de Oliveira <hercules.oliveira@syngenta.com>
2023-11-22 21:03:25 -0300

    chore: added map in user projects

--- src/components/UserProjects/index.tsx
+++ src/components/UserProjects/index.tsx
@@ -3,38 +3,32 @@ import imageSyngenta from '../../assets/images/syngenta.png'
 import imageCoral from '../../assets/images/coral.png'
 
 export const UserProjects = () => {
+  const projectsData = [
+    {
+      image: imageSyngenta,
+      title: 'Syngenta Digital',
+      description:
+        'Tenho atuado diretamente com dados referentes a qualidade do plantio de fazendas para trazer uma melhor visualicação e otimização para grandes produtores.'
+    },
+    {
+      image: imageCoral,
+      title: 'Coral Tintas',
+      description:
+        'Atuei com a crianção do ecommerce totalmente do zero utilizando VTEX.'
+    }
+  ]
   return (
     <section className="flex flex-col items-center">
-      <h3>Principais Projetos</h3>
-      <Card className="p-0 justify-start max-w-md max-h-40">
-        <img
-          className="h-full rounded-md"
-          src={imageSyngenta}
-          alt="logo da syngenta digital"
-        />
-        <div>
-          <h4 className="pl-2 py-2 text-black">Cropwise Analytics</h4>
-          <p className="pl-2">
-            Tenho atuado diretamente com dados referentes a qualidade do plantio
-            de fazendas para trazer uma melhor visualicação e otimização para
-            grandes produtores.
-          </p>
-        </div>
-      </Card>
-      <Card className="p-0 justify-start max-w-md max-h-40">
-        <img
-          className="h-full w-40 rounded-md"
-          src={imageCoral}
-          alt="logo da syngenta digital"
-        />
-        <div>
-          <h4 className="pl-2 py-2 text-black">Coral Tintas</h4>
-          <p className="pl-2">
-            Atuei com a crianção do ecommerce totalmente do zero utilizando
-            VTEX.
-          </p>
-        </div>
-      </Card>
+      <h3 className="text-black  text-xl">Principais Projetos</h3>
+      {projectsData.map(project => (
+        <Card className="p-0 justify-start max-w-md max-h-40">
+          <img className="h-full w-40 rounded-md" src={project.image} />
+          <div>
+            <h4 className="pl-2 py-2 text-black">{project.title}</h4>
+            <p className="pl-2">{project.description}</p>
+          </div>
+        </Card>
+      ))}
     </section>
   )
 }
