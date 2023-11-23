2bd1c8884aadb06e36fc9297b77d9ce287b4cb83
Hercules de Oliveira <hercules.oliveira@syngenta.com>
2023-11-22 21:04:35 -0300

    feat: user contacts component and added link to redirect

--- src/components/UserContacts/index.tsx
+++ src/components/UserContacts/index.tsx
@@ -1,22 +1,43 @@
 import { Card } from '../../components/Card'
-import { FaLinkedinIn, FaGithub, FaWhatsapp } from "react-icons/fa";
+import { FaLinkedinIn, FaGithub, FaWhatsapp } from 'react-icons/fa'
 
-export const UserContacts = () => {
+interface UserContactsProps {
+  linkendinUrl: string
+  githubUrl: string
+  whatsappNumber: string
+}
+
+export const UserContacts = ({
+  linkendinUrl,
+  githubUrl,
+  whatsappNumber
+}: UserContactsProps) => {
   return (
-      <section className="flex flex-col min-w-300 items-center">
-        <Card>
-          <FaLinkedinIn />
-          <h1>Linkedin</h1>
-        </Card>
-        <Card>
-          <FaGithub />
+    <section className="flex flex-col min-w-300 items-center">
+      <h3 className="text-black text-xl">Contatos</h3>
+      <Card className="gap-2 items-center hover:bg-blue-400">
+        <FaLinkedinIn />
+        <h1>
+          <a
+            href={`https://www.linkedin.com/in/${linkendinUrl}`}
+            target="blank"
+          >
+            Linkedin
+          </a>
+        </h1>
+      </Card>
+      <Card className="gap-2 items-center hover:bg-gray-800">
+        <FaGithub />
+        <a href={`https://github.com/${githubUrl}`} target="blank">
           <h1>Github</h1>
-        </Card>
-        <Card>
-          <FaWhatsapp />
+        </a>
+      </Card>
+      <Card className="gap-2 items-center hover:bg-green-400">
+        <FaWhatsapp />
+        <a href={`https://wa.me/${whatsappNumber}`} target="blank">
           <h1>Contate-me</h1>
-        </Card>
-        
-      </section>
+        </a>
+      </Card>
+    </section>
   )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,3 @@
-import { Card } from '../../components/Card'
 import { UserProfile } from '../../components/Title'
 import { UserContacts } from '../../components/UserContacts'
 import { UserImage } from '../../components/UserImage'
@@ -8,13 +7,21 @@ interface HomeProps {
   username: string
 }
 
+const linkedinUrl = '/hercules-oliveira-18599b1b0/'
+const githubUrl = 'herculesoliverr'
+const whatsappNumber = '5531993563361'
+
 export const Home = ({ username }: HomeProps) => {
   return (
     <div className="flex items-center flex-col h-screen overflow-auto">
       <UserImage username={username} />
       <UserProfile username={username} />
       <UserProjects />
-      <UserContacts />
+      <UserContacts
+        githubUrl={githubUrl}
+        linkendinUrl={linkedinUrl}
+        whatsappNumber={whatsappNumber}
+      />
     </div>
   )
 }
